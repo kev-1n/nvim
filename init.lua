2d43f3d39005d63bad9de2ee7dc7ad375aa0aa2c
Kevin Tapia <kevin.tapia@latam.com>
2025-01-03 14:55:38 -0300

    ADD ruby_lsp

--- init.lua
+++ init.lua
@@ -622,6 +622,7 @@ require('lazy').setup({
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
+        ruby_lsp = {},
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
